924a8d254f44fc0eae84046c09ddef46f760fd32
christine.ho@coatesgroup.com <christineyinho@gmail.com>
2020-12-06 16:15:01 +1100

    fix information coming from rest countries api

--- client/src/components/Country/index.js
+++ client/src/components/Country/index.js
@@ -4,18 +4,24 @@ import axios from 'axios';
 export const Country = ({ match }) => {
     const [country, setData] = useState([]);
 
-    const [ideas, setIdeas] = useState([]);
+    const [countryInformation, setCountryInformation] = useState([]);
 
     const {
         params: { locationId },
     } = match;
 
+    const getCountryInformation = async (name) => {
+        const result = await axios(`https://restcountries.eu/rest/v2/name/${name}`);
+
+        setCountryInformation(result.data);
+    }
+
     useEffect(() => {
         const fetchData = async () => {
-            console.log(match)
             const result = await axios(`http://localhost:8000/api/locations/${locationId}/`);
 
             setData(result.data);
+            getCountryInformation(result.data.name);
         };
        
         fetchData();
@@ -25,7 +31,39 @@ export const Country = ({ match }) => {
         <div>
             <h1>Country</h1>
             {
-                <p>{ country.name }</p>
+                countryInformation.length > 0 && 
+                <div>
+                    { 
+                        countryInformation.map((location, index) =>
+                            <div className="country-information" key={index}>
+                                <h1>{ location.name }</h1>
+                                <img src={ location.flag } alt={`${country.name} flag`} />
+                                <span>
+                                    Currencies: { 
+                                        location.currencies.length > 1 
+                                        ? location.currencies.map((currency, index) => (
+                                            <span key={index}>
+                                                <p>{ currency.code }</p>
+                                            </span>
+                                        ))
+                                        : <p>{ location.currencies[0].code }</p>
+                                    }
+                                </span>
+                                <span>
+                                    Languages: { 
+                                        location.languages.length > 1 
+                                        ? location.languages.map((language) => (
+                                            <span key={index}>
+                                                <p>{ language.name }</p>
+                                            </span>
+                                        ))
+                                        : <p>{ location.languages[0] }</p>
+                                    }
+                                </span>
+                            </div>
+                        )
+                    }
+                </div>
             }
         </div>
     )
